925aed15560baf8c906cd2a0f25e189be2eaf595
chakri <chanty.chakravarthy@gmail.com>
2017-02-28 18:07:47 +0530

    sorted functions

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function getWeather(lat, lon) {
-  let temp;
-  const apikey = "baf6c6853241d66bcc45614cd1e3e901";
-  const url = "http://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
-  let xhr = new XMLHttpRequest();
-  console.log(url);
-  xhr.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      let jsondata = xhr.responseText;
-      let data = JSON.parse(jsondata);
-      temp = data.main.temp;
-      console.log(data.main.temp);
-    }
-  };
-  return temp;
-  xhr.open("GET", url, true);
-  xhr.send();
-}
-
-function success(position) {
-  getWeather(position.coords.latitude, position.coords.longitude);
-};
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success);
-} else {
-  console.log('Browser Does not support location access or location permission is blocked');
-}
-
 class CurrentTemp extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      temp: '10',
-      location: 'Vijayawada'
-    }
-  }
-  
   render() {
     return (
       <div className="currentTemp">
         <span className="temp">
-          {this.state.temp}&deg;
+          {this.props.temp}&deg;
         </span>
         <span className="location">
-          {this.state.location}
+          {this.props.location}
         </span>
       </div>
     );
@@ -85,11 +47,53 @@ class LocationUnavailable extends React.Component {
 }
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: '10',
+      location: 'Vijayawada'
+    };
+  }
+
+  getWeather() {
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      console.log('Browser Does not support location access or location permission is blocked');
+    }
+
+    function success(position) {
+      let temp;
+      const apikey = "baf6c6853241d66bcc45614cd1e3e901";
+      const url = "http://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + apikey;
+      let xhr = new XMLHttpRequest();
+      console.log(url);
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          let jsondata = xhr.responseText;
+          let data = JSON.parse(jsondata);
+          temp = data.main.temp;
+          console.log(temp);
+        }
+      };
+      xhr.open("GET", url, true);
+      xhr.send();
+    };
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
   render() {
     return (
       <div>
         <div className="weatherCard">
-          <CurrentTemp />
+          <CurrentTemp
+            temp={this.state.temp}
+            location={this.state.location} />
           <CurrentWeather />
         </div>
         {/* <LocationUnavailable /> */}
