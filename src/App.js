57809b396d31ca95cc0834099de1839b5b11c583
chakri <chanty.chakravarthy@gmail.com>
2017-03-01 11:57:43 +0530

    Working version 1 :yay:

--- src/App.js
+++ src/App.js
@@ -17,18 +17,14 @@ class CurrentTemp extends React.Component {
 }
 
 class CurrentWeather extends React.Component {
+
   render () {
     return (
       <div className="currentWeather">
-        <span className="conditions">
-          &#xf00d;
-        </span>
+        <span className={"conditions "+ this.props.icon}></span>
         <div className="info">
-          <span className="rain">
-            1.3 MM
-          </span>
-          <span className="wind">
-            10 MPH
+          <span className="description">
+            	{this.props.description}
           </span>
         </div>
       </div>
@@ -51,8 +47,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      temp: '10',
-      location: 'Vijayawada'
+      temp: '',
+      location: '',
+      icon: '',
+      description: ''
     };
   }
 
@@ -64,23 +62,34 @@ class App extends Component {
       console.log('Browser Does not support location access or location permission is blocked');
     }
 
+	  let self = this;
+
     function success(position) {
-      let temp;
       const apikey = "baf6c6853241d66bcc45614cd1e3e901";
       const url = "http://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + apikey;
       let xhr = new XMLHttpRequest();
       console.log(url);
       xhr.onreadystatechange = function() {
+	      let temp, location, icon, description;
         if (this.readyState === 4 && this.status === 200) {
           let jsondata = xhr.responseText;
           let data = JSON.parse(jsondata);
-          temp = data.main.temp;
-          console.log(temp);
+          temp = parseInt(data.main.temp);
+          location = data.name;
+          icon = data.weather[0].id;
+          description = data.weather[0].description;
+          console.log(icon, description);
+	        self.setState({
+		        temp: temp,
+		        location: location,
+            icon: "wi-owm-"+icon,
+            description: description
+	        });
         }
       };
       xhr.open("GET", url, true);
       xhr.send();
-    };
+    }
   }
 
   componentDidMount() {
@@ -94,7 +103,9 @@ class App extends Component {
           <CurrentTemp
             temp={this.state.temp}
             location={this.state.location} />
-          <CurrentWeather />
+          <CurrentWeather
+            icon={this.state.icon}
+            description={this.state.description} />
         </div>
         {/* <LocationUnavailable /> */}
       </div>
